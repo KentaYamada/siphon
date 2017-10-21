20d59a1322e3f558d8478fccec4bc74e4aab9809
Kenta Yamada <knt.yamada.800@gamil.com>
2017-10-21 22:13:04 +0900

    [Add] 商品カテゴリ取得ストアド作成

---
+++ sql/functions/find_all_categories.sql
@@ -0,0 +1,14 @@
+DROP FUNCTION IF EXISTS find_all_categories();
+
+CREATE OR REPLACE FUNCTION find_all_categories()
+RETURNS TABLE(id integer, name text)
+LANGUAGE plpgsql
+SECURITY DEFINER
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        c.id
+       ,c.name
+    FROM categories AS c;
+END $$;
