986e52190be8843df45149bccfa967646ddfc307
Kenta Yamada <knt.yamada.800@gamil.com>
2017-11-06 08:51:46 +0900

    [Add] テストデータ作成

---
+++ sql/tests/data/product.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (category_id, order_no, name, price)
+    VALUES (1, 1, 'Siphon coffee', 450),
+           (1, 2, 'Ice coffee', 450),
+           (1, 3, 'Tea', 450),
+           (2, 1, 'Morning set', 550),
+           (2, 2, 'Lunch set', 750),
+           (2, 3, 'Dinner set', 1050);
+
