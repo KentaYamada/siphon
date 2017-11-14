5b558e5f4ee23c9b76613bb9a306c352a57b5e47
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2017-11-15 08:45:08 +0900

    [Upd] プロトタイピングレベルの実装

--- siphon/models/pgadapter.py
+++ siphon/models/pgadapter.py
@@ -1,31 +1,68 @@
 # -*- coding: utf-8 -*-
-#
-# Siphon
-# pypg.py
-# This program wrapped psycopg2.
-#
-# import logging
+
 import psycopg2
-import psycopg2.extensions
 import psycopg2.extras
 
 
-class Pypg():
-    def __init__(self, dsn=''):
-        # Todo: dsn set config from config
+class PgAdapter():
+    def __init__(self, dsn):
         self.__con = None
-        self.__cur = None
         self.__dsn = dsn
 
-    def __execute(self, query, args):
-        if not query:
-            raise ValueError("query must be set value")
-        if args is None:
-            raise ValueError("args must be set value")
+    def __exec(self, query, args):
         self.__con = psycopg2.connect(self.__dsn)
-        self.__cur = self.__con.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        self.__cur.execute(query, args)
+        cur = self.__con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.callproc(query, args)
+        return cur
 
-    def detatch(self):
-        if self.__con is None:
+    @property
+    def connected(self):
+        return True if self.__con.closed < 1 else False
+
+    def execute_script(self, query):
+        pass
+
+    def save(self, query, args):
+        cur = self.__exec(query, args)
+        print(cur.query)
+        return True if cur.rowcount > 0 else False
+
+    def remove(self, query, args):
+        cur = self.__exec(query, args)
+        print(cur.query)
+        return True if cur.rowcount > 0 else False
+
+    def find(self, query, args=None):
+        rows = None
+        try:
+            cur = self.__exec(query, args)
+            print(cur.query)
+            rows = [row for row in cur.fetchall()]
+        finally:
             self.__con.close()
+        return rows
+
+    def find_one(self, query, args):
+        row = None
+        try:
+            cur = self.__exec(query, args)
+            print(cur.query)
+            row = cur.fetchone()
+        finally:
+            self.__con.close()
+        return row
+
+    def commit(self):
+        if self.__con is not None:
+            self.__con.commit()
+            self.__con.close()
+
+    def rollback(self):
+        if self.__con is not None:
+            self.__con.rollback()
+            self.__con.close()
+
+    @classmethod
+    def read_sqlfile(cls, filename):
+        pass
+
