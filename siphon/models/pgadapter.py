2c5c015cefe04c5dfc6154d37e3213029c794df9
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2017-11-19 17:15:10 +0900

    [Fix] コマンド実行ロジック修正
    SQL直実行できるように修正

--- siphon/models/pgadapter.py
+++ siphon/models/pgadapter.py
@@ -9,10 +9,17 @@ class PgAdapter():
         self.__con = None
         self.__dsn = dsn
 
+    def __build_find_command(self, query):
+        return "SELECT * FROM {0};".format(query)
+
+    def __build_exec_command(self, query):
+        return "SELECT {0};".format(query)
+
     def __exec(self, query, args):
         self.__con = psycopg2.connect(self.__dsn)
         cur = self.__con.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.callproc(query, args)
+        cur.execute(query, args)
+        print(cur.query)
         return cur
 
     @property
@@ -20,23 +27,21 @@ class PgAdapter():
         return True if self.__con.closed < 1 else False
 
     def execute_script(self, query):
-        pass
+        self.__exec(cmd, None)
 
     def save(self, query, args):
-        cur = self.__exec(query, args)
-        print(cur.query)
+        cmd = self.__build_exec_command(query)
+        cur = self.__exec(cmd, args)
         return True if cur.rowcount > 0 else False
 
     def remove(self, query, args):
-        cur = self.__exec(query, args)
-        print(cur.query)
-        return True if cur.rowcount > 0 else False
+        return self.save(query, args)
 
     def find(self, query, args=None):
         rows = None
+        cmd = self.__build_find_command(query)
         try:
-            cur = self.__exec(query, args)
-            print(cur.query)
+            cur = self.__exec(cmd, args)
             rows = [row for row in cur.fetchall()]
         finally:
             self.__con.close()
@@ -44,9 +49,9 @@ class PgAdapter():
 
     def find_one(self, query, args):
         row = None
+        cmd = self.__build_find_command(query)
         try:
-            cur = self.__exec(query, args)
-            print(cur.query)
+            cur = self.__exec(cmd, args)
             row = cur.fetchone()
         finally:
             self.__con.close()
@@ -65,4 +70,3 @@ class PgAdapter():
     @classmethod
     def read_sqlfile(cls, filename):
         pass
-
