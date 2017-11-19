dedfa1744564e1c51a5f7bb26a782b2dc2df6d94
Kenta Yamada <kntx.fullspeed.428@gmail.com>
2017-11-19 20:50:51 +0900

    -F-

---
+++ siphon/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+DEVELOP_DB = {
+    'host': 'localhost',
+    'dbname': 'siphon_dev',
+    'user': 'kenta',
+    'password': 'kenta',
+}
+
+TEST_DB = {
+    'host': 'localhost',
+    'dbname': 'siphon_dev',
+    'user': 'kenta',
+    'password': 'kenta',
+}
