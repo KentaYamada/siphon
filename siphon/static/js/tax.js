8a229e9c563daee58b168362f7a4a46d582a6449
Kenta Yamada <knt.yamada.800@gamil.com>
2017-12-07 22:27:56 +0900

    [Upd] リファクタリング
    APIリクエスト部分をModelへ移植

--- siphon/static/js/tax.js
+++ siphon/static/js/tax.js
@@ -1,24 +1,35 @@
 "use strict";
 
 
-var Tax = function(rate=1, tax_type='out') {
+var Tax = function(rate, tax_type) {
     var self = this;
+    self.END_POINT = "/api/tax";
     self.rate = ko.observable(rate);
     self.tax_type = ko.observable(tax_type);
+
+    self.edit = function() {
+        requestApi(self.END_POINT, 'POST', ko.toJSON(self.tax())).done(function(data) {
+            console.log(data);
+        });
+    }
+
+    self.find = function(model) {
+        requestApi(self.END_POINT, 'GET').done(function(data) {
+            model(new Tax(data.tax.rate, data.tax.tax_type));
+        });
+    }
+
 }
 
 
 var TaxViewModel = function() {
     var self = this;
     self.tax = ko.observable();
-    requestApi('/api/tax', 'GET').done(function(data) {
-        self.tax(new Tax(data.tax.rate, data.tax.tax_type));
-    });
+    var model = new Tax();
+    mode.find(self.tax);
 
     self.onSave = function() {
-        requestApi('/api/tax', 'POST', ko.toJSON(self.tax())).done(function(data) {
-            console.log('succeed.');
-        });
+        self.tax().edit();
     }
 }
 
