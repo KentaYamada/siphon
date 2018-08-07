4d01184de2253e954d0dfb95768264b04aa40a3a
Kenta Yamada <knt.yamada.800@gamil.com>
2018-08-07 18:54:03 +0900

    24 During the implements frontend

--- app/controller/view.py
+++ app/controller/view.py
@@ -4,6 +4,11 @@ from flask import render_template, Blueprint
 bp = Blueprint('view', __name__, template_folder='templates')
 
 
+@bp.route('/categories', methods=['GET'])
+def categories():
+    return render_template('category_list.html')
+
+
 @bp.route('/users', methods=['GET'])
 def users():
     return render_template('user_list.html')
