6c752f4523b292a15ddf856857492ab26002c47e
Kenta Yamada <knt.yamada.800@gamil.com>
2018-08-23 21:38:02 +0900

    #63 refactoring HTTP status method name

--- app/model/response.py
+++ app/model/response.py
@@ -20,14 +20,14 @@ class ResponseBodyCreator():
             'status_code': None
         }
 
-    def __get_success_response(self, data, status_code, message):
+    def get_success_response(self, data, status_code, message):
         self.__response['message'] = message
         self.__response['status_code'] = status_code
         if data is not None:
             self.__response.update(data)
         return self.__response
 
-    def __get_failed_response(self, errors, status_code, message):
+    def get_failed_response(self, errors, status_code, message):
         if errors is None:
             raise ValueError()
         self.__response['errors'] = errors
@@ -35,72 +35,50 @@ class ResponseBodyCreator():
         self.__response['status_code'] = status_code
         return self.__response
 
-    def added(self, data, status_code=CREATED, message=''):
-        """ get response body if added """
+    def ok(self, data, message=''):
         if not message:
-            message = '登録しました。'
-        return self.__get_success_response(data, status_code, message)
+            message = '処理成功しました'
+        return self.get_success_response(data, OK, message)
 
-    def add_failed(self, errors, status_code=BAD_REQUEST, message=''):
-        """ get response body if add failed """
+    def created(self, data, message=''):
         if not message:
-            message = '登録に失敗しました'
-        return self.__get_failed_response(errors, status_code, message)
+            message = '登録しました'
+        return self.get_success_response(data, CREATED, message)
 
-    def edited(self, data, status_code=OK, message=''):
-        """ get response body if updated """
-        if not message:
-            message = '変更しました'
-        return self.__get_success_response(data, status_code, message)
-
-    def edit_failed(self, errors, status_code=CONFLICT, message=''):
-        """ get response body if update failed """
-        if not message:
-            message = '変更に失敗しました'
-        return self.__get_failed_response(errors, status_code, message)
-
-    def deleted(self, status_code=NO_CONTENT, message=''):
-        """ get response body if deleted """
+    def no_content(self, message=''):
         if not message:
             message = '削除しました'
-        return self.__get_success_response(None, status_code, message)
+        return self.get_success_response(None, NO_CONTENT, message)
 
-    def delete_failed(self, errors, status_code=CONFLICT, message=''):
-        """ get response body if delete failed """
+    def moved_marmently(self, data, message=''):
         if not message:
-            message = '削除に失敗しました'
-        return self.__get_failed_response(errors, status_code, message)
+            message = ''
+        return self.get_success_response(data, MOVED_PARMANENTLY, message)
 
-    def fetch_success(self, data, status_code=OK, message=''):
-        """ get response body if fetched """
+    def bad_request(self, errors, message=''):
         if not message:
-            message = 'データ取得成功'
-        return self.__get_success_response(data, status_code, message)
-
-    def fetch_failed(self, errors, status_code, message=''):
-        """ get response body if fetch failed"""
-        raise NotImplementedError()
-
-    def fetch_no_data(self, status_code=NO_CONTENT, message=''):
-        """ get response if fetch no data """
-        if not message:
-            message = 'データがありません'
-        return self.__get_success_response((), status_code, message)
-
-    def notfound(self, message=''):
-        """ get response if resource not found """
-        if not message:
-            message = '要求されたリソースは見つかりませんでした'
-        return self.__get_failed_response((), NOT_FOUND, message)
+            message = '不正なリクエストです'
+        return self.get_failed_response(errors, BAD_REQUEST, message)
 
     def unauthorized(self, message=''):
-        """ get response if unauthorized request """
         if not message:
             message = '認証されていません'
-        return self.__get_failed_response((), UNAUTHORIZED, message)
+        return self.get_failed_response((), UNAUTHORIZED, message)
+
+    def forbidden(self, message=''):
+        raise NotImplementedError()
+
+    def not_found(self, message=''):
+        if not message:
+            message = '要求されたリソースは見つかりませんでした'
+        return self.get_failed_response((), NOT_FOUND, message)
+
+    def conflict(self, errors, message=''):
+        if not message:
+            message = '変更に失敗しました'
+        return self.get_failed_response(errors, CONFLICT, message)
 
     def internal_server_error(self, message=''):
-        """ get response if exceptions """
         if not message:
             message = 'サーバープログラムでエラーが発生しました'
-        return self.__get_failed_response((), INTERNAL_SERVER_ERROR, message)
+        return self.get_failed_response((), INTERNAL_SERVER_ERROR, message)
