b00ab795c8706d89ebe3cce1a029b07dd33c9924
Kenta Yamada <knt.yamada.800@gamil.com>
2018-10-04 18:18:16 +0900

    #24 商品カテゴリのDB連携

--- app/model/category.py
+++ app/model/category.py
@@ -1,4 +1,10 @@
+from app.model.pgadapter import PgAdapter
+from app.config import get_db_config
+
+
 class Category():
+    db = PgAdapter(get_db_config('develop'))
+
     def __init__(self, id=None, name=''):
         self.__id = id
         self.__name = name
@@ -19,18 +25,50 @@ class Category():
     def save(self):
         if not self.validate():
             return False
-        return True
+        saved = False
+        try:
+            Category.db.save('save_category', (self.__id, self.__name))
+            saved = True if Category.db.affected_rows == 1 else False
+            if saved:
+                Category.db.commit()
+            else:
+                Category.db.rollback()
+        except Exception as e:
+            Category.db.rollback()
+            print(e)
+            raise e
+        return saved
 
     def delete(self):
         if self.__id is None:
             return False
-        return True
+        deleted = False
+        try:
+            Category.db.remove('delete_category', (self.__id,))
+            deleted = True if Category.db.affected_rows == 1 else False
+            if deleted:
+                Category.db.commit()
+            else:
+                Category.db.rollback()
+        except Exception as e:
+            Category.db.rollback()
+            print(e)
+            raise e
+        return deleted
 
     @classmethod
     def find_all(cls):
-        return [
-            {'id': i, 'name': 'Category{}'.format(i)}
-            for i in range(1, 10)]
+        categories = []
+        try:
+            rows = Category.db.find('find_categories')
+            if len(rows) > 0:
+                categories = [
+                    {'id': row['id'], 'name': row['name']} for row in rows]
+        except Exception as e:
+            print(e)
+            raise e
+        print(categories)
+        return categories
 
     def __set_error(self, field, message):
         if field in self.__errors:
