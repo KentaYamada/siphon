29253a1b3e6eed35655ed28e77b0c23b525102fd
Kenta Yamada <knt.yamada.800@gamil.com>
2018-10-14 15:03:14 +0900

    #78 refactoring comment

--- app/model/pgadapter.py
+++ app/model/pgadapter.py
@@ -14,9 +14,7 @@ class PgAdapter():
         self.__cur = None
 
     def __create_cursor(self):
-        """
-            attach database and create cursor
-        """
+        """ attach database and create cursor """
         if self.__config is None:
             raise ValueError()
         if self.__con is None or self.__con.closed:
@@ -34,7 +32,7 @@ class PgAdapter():
         return self.__cur.rowcount
 
     def execute_proc(self, command, data=None):
-        """ execute stored procedure. """
+        """ execute stored procedure """
         if not command:
             raise ValueError()
         self.__create_cursor()
@@ -42,9 +40,7 @@ class PgAdapter():
         return self.__cur.rowcount
 
     def find(self, command, condition=None):
-        """
-            execute find rows procedure
-        """
+        """ execute find rows procedure """
         if not command:
             raise ValueError()
         self.__create_cursor()
@@ -52,9 +48,7 @@ class PgAdapter():
         return self.__cur.fetchall()
 
     def find_one(self, command, condition):
-        """
-            execute find row procedure
-        """
+        """ execute find row procedure """
         if not command:
             raise ValueError()
         self.__create_cursor()
@@ -62,9 +56,7 @@ class PgAdapter():
         return self.__cur.fetchone()
 
     def fetch_rowcount(self, tablename):
-        """
-            fetch row count by specific table
-        """
+        """ fetch row count by specific table """
         if not tablename:
             raise ValueError()
         query = 'SELECT COUNT(id) AS rowcount FROM {0};'.format(tablename)
@@ -73,17 +65,13 @@ class PgAdapter():
         return self.__cur.fetchone()['rowcount']
 
     def fetch_last_row_id(self):
-        """
-            fetch last row id
-        """
+        """ fetch last row id """
         self.__create_cursor()
         self.__cur.execute('SELECT LASTVAL();')
         return self.__cur.fetchone()['lastval']
 
     def bulk_insert(self, query, values):
-        """
-            run bulk insert values
-        """
+        """ run bulk insert values """
         if not query:
             raise ValueError()
         if values is None or len(values) < 1:
@@ -93,18 +81,14 @@ class PgAdapter():
         return self.__cur.rowcount
 
     def commit(self):
-        """
-            commit transaction
-        """
+        """ commit transaction """
         if self.__con is not None and not self.__con.closed:
             self.__con.commit()
             self.__cur.close()
             self.__con.close()
 
     def rollback(self):
-        """
-            rollback transaction
-        """
+        """ rollback transaction """
         if self.__con is not None and not self.__con.closed:
             self.__con.rollback()
             self.__cur.close()
