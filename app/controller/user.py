00e041feb8079daec99af8049c51bc5e0af1e902
Kenta Yamada <knt.yamada.800@gamil.com>
2019-01-03 18:22:14 +0900

    #84 refactoring

--- app/controller/user.py
+++ app/controller/user.py
@@ -1,7 +1,7 @@
-from flask import jsonify, request, Blueprint
+from flask import request, Blueprint
+from app.controller.response import ResponseBody
 from app.model.user import User
 from app.model.mapper.user_mapper import UserMapper
-from app.model.response import ResponseBodyCreator
 
 
 bp = Blueprint('user', __name__, url_prefix='/api/users')
@@ -9,99 +9,81 @@ bp = Blueprint('user', __name__, url_prefix='/api/users')
 
 @bp.route('/', methods=['GET'])
 def index():
-    body_createor = ResponseBodyCreator()
-    body = body_createor.ok(User.find_by(''))
-    res = jsonify(body)
-    res.status_code = body['status_code']
+    res = ResponseBody()
+    users = User.find_by('')
+    res.set_success_response(200, users)
     return res
 
 
 @bp.route('/', methods=['POST'])
 def add():
-    body_createor = ResponseBodyCreator()
+    res = ResponseBody()
 
     if request.json is None:
-        body = body_createor.bad_request()
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400)
         return res
 
     user = User(None, **request.json)
+
     if not user.is_valid():
-        body = body_createor.bad_request(user.validation_errors)
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400, user.validation_errors)
         return res
 
     mapper = UserMapper()
     saved = mapper.add(user)
 
     if saved:
-        body = body_createor.created(request.json)
-
-    res = jsonify(body)
-    res.status_code = body['status_code']
+        res.set_success_response(201)
+    else:
+        res.set_fail_response(409)
     return res
 
 
 @bp.route('/<int:id>', methods=['PUT'])
 def edit(id):
-    body_createor = ResponseBodyCreator()
+    res = ResponseBody()
     user = User(id, **request.json)
 
     if not user.is_valid():
-        body = body_createor.bad_request(user.validation_errors)
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400)
         return res
 
     mapper = UserMapper()
     saved = mapper.edit(user)
 
     if saved:
-        body = body_createor.ok(request.json)
-
-    res = jsonify(body)
-    res.status_code = body['status_code']
+        res.set_success_response(200, message='更新しました')
+    else:
+        res.set_fail_response(409)
     return res
 
 
 @bp.route('/<int:id>', methods=['DELETE'])
 def delete(id):
+    res = ResponseBody()
     mapper = UserMapper()
     deleted = mapper.delete(id)
-    body_createor = ResponseBodyCreator()
 
     if deleted:
-        body = body_createor.no_content('delete successfuly')
+        res.set_success_response(204)
     else:
-        body = body_createor.not_found('resource not found')
-
-    res = jsonify(body)
-    res.status_code = body['status_code']
+        res.set_success_response(404)
     return res
 
 
 @bp.route('/authoricate', methods=['POST'])
 def authoricate():
-    body_createor = ResponseBodyCreator()
+    res = ResponseBody()
 
     if request.json is None:
-        body = body_createor.bad_request(
-            ['空のリクエストデータです']
-        )
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400, message='空のリクエストデータです')
         return res
 
     mapper = UserMapper()
     can_login = mapper.authoricate(**request.json)
 
     if can_login:
-        body = body_createor.ok(None, 'authorication successfuly')
+        res.set_success_response(200, message='認証成功')
     else:
-        body = body_createor.unauthorized('failed authorication')
-
-    res = jsonify(body)
-    res.status_code = body['status_code']
+        res.set_fail_response(401, message='認証失敗')
     return res
