ee6543f4912ef4c86ac0697feef684473e6c5937
Kenta Yamada <knt.yamada.800@gamil.com>
2019-01-03 18:02:08 +0900

    #84 refactoring

--- app/controller/category.py
+++ app/controller/category.py
@@ -1,7 +1,7 @@
-from flask import jsonify, request, Blueprint
+from flask import request, Blueprint
+from app.controller.response import ResponseBody
 from app.model.category import Category
 from app.model.mapper.category_mapper import CategoryMapper
-from app.model.response import ResponseBodyCreator
 
 
 bp = Blueprint('category', __name__, url_prefix='/api/categories')
@@ -9,86 +9,68 @@ bp = Blueprint('category', __name__, url_prefix='/api/categories')
 
 @bp.route('/', methods=['GET'])
 def index():
-    body_createor = ResponseBodyCreator()
-    body = body_createor.ok({'categories': Category.find_all()})
-    res = jsonify(body)
-    res.status_code = body['status_code']
+    categories = Category.find_all()
+    res = ResponseBody()
+    res.set_success_response(200, categories)
     return res
 
 
 @bp.route('/', methods=['POST'])
 def add():
-    body_creator = ResponseBodyCreator()
+    res = ResponseBody()
 
     if request.json is None:
-        body = body_creator.bad_request(None)
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400)
         return res
 
     category = Category(**request.json)
 
     if not category.is_valid():
-        body = body_creator.bad_request(category.validation_errors)
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400, category.validation_errors)
         return res
 
     mapper = CategoryMapper()
     saved = mapper.add(category)
 
     if saved:
-        body = body_creator.created(request.json)
-    elif not saved and category.validation_errors:
-        body = body_creator.bad_request(category.validation_errors)
-
-    res = jsonify(body)
-    res.status_code = body['status_code']
+        res.set_success_response(201)
+    else:
+        res.set_fail_response(409)
     return res
 
 
 @bp.route('/<int:id>', methods=['PUT'])
 def edit(id):
-    body_creator = ResponseBodyCreator()
+    res = ResponseBody()
 
     if request.json is None:
-        body = body_creator.bad_request()
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400)
         return res
 
     category = Category(id, **request.json)
 
     if not category.is_valid():
-        body = body_creator.bad_request(category.validation_errors)
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400)
         return res
 
     mapper = CategoryMapper()
     saved = mapper.edit(category)
 
     if saved:
-        body = body_creator.ok({'data': request.json}, '更新しました')
-    elif not saved and category.validation_errors:
-        body = body_creator.conflict(category.validation_errors)
-
-    res = jsonify(body)
-    res.status_code = body['status_code']
+        res.set_success_response(200, message='更新しました')
+    else:
+        res.set_fail_response(409)
     return res
 
 
 @bp.route('/<int:id>', methods=['DELETE'])
 def delete(id):
+    res = ResponseBody()
     mapper = CategoryMapper()
     deleted = mapper.delete(id)
-    body_creator = ResponseBodyCreator()
 
     if deleted:
-        body = body_creator.no_content()
+        res.set_success_response(204)
     else:
-        body = body_creator.not_found()
-
-    res = jsonify(body)
-    res.status_code = body['status_code']
+        res.set_success_response(404)
     return res
