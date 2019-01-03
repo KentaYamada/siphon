bffd78937a8ec860ff7a7e04b8804368fd7bf5f7
Kenta Yamada <knt.yamada.800@gamil.com>
2019-01-03 18:14:57 +0900

    #84 refactoring

--- app/controller/item.py
+++ app/controller/item.py
@@ -1,7 +1,7 @@
-from flask import request, jsonify, Blueprint
+from flask import request, Blueprint
+from app.controller.response import ResponseBody
 from app.model.item import Item
 from app.model.mapper.item_mapper import ItemMapper
-from app.model.response import ResponseBodyCreator
 
 
 bp = Blueprint('item', __name__, url_prefix='/api/items')
@@ -14,84 +14,66 @@ def index():
     else:
         category_id = request.args.get('category_id')
     items = Item.find_by(category_id)
-    body_creator = ResponseBodyCreator()
-    body = body_creator.ok({'items': items}, message='データ取得成功')
-    res = jsonify(body)
-    res.status_code = body['status_code']
+    res = ResponseBody()
+    res.set_success_response(200, {'items': items})
     return res
 
 
 @bp.route('/', methods=['POST'])
 def add():
-    body_creator = ResponseBodyCreator()
+    res = ResponseBody()
 
     if request.json is None:
-        body = body_creator.bad_request(None)
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400)
         return res
 
     item = Item(**request.json)
+
     if not item.is_valid():
-        body = body_creator.bad_request(item.validation_errors)
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400, item.validation_errors)
         return res
 
     mapper = ItemMapper()
     saved = mapper.add(item)
 
     if saved:
-        body = body_creator.created(request.json)
-    elif not saved and item.errors:
-        body = body_creator.bad_request(item.errors)
-
-    res = jsonify(body)
-    res.status_code = body['status_code']
+        res.set_success_response(201)
+    else:
+        res.set_fail_response(409)
     return res
 
 
 @bp.route('/<int:id>', methods=['PUT'])
 def edit(id):
-    body_creator = ResponseBodyCreator()
+    res = ResponseBody()
 
     if request.json is None:
-        body = body_creator.bad_request()
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400)
         return res
 
     item = Item(**request.json)
     if not item.is_valid():
-        body = body_creator.bad_request(item.validation_errors)
-        res = jsonify(body)
-        res.status_code = body['status_code']
+        res.set_fail_response(400, item.validation_errors)
         return res
 
     mapper = ItemMapper()
     saved = mapper.add(item)
 
     if saved:
-        body = body_creator.ok(request.json)
-    elif not saved and len(item.errors) > 0:
-        body = body_creator.conflict(item.errors)
-
-    res = jsonify(body)
-    res.status_code = body['status_code']
+        res.set_success_response(200, message='更新しました')
+    else:
+        res.set_fail_response(409)
     return res
 
 
 @bp.route('/<int:id>', methods=['DELETE'])
 def delete(id):
+    res = ResponseBody()
     mapper = ItemMapper()
     deleted = mapper.delete(id)
-    body_creator = ResponseBodyCreator()
 
     if deleted:
-        body = body_creator.no_content()
+        res.set_success_response(204)
     else:
-        body = body_creator.not_found()
-
-    res = jsonify(body)
-    res.status_code = body['status_code']
+        res.set_success_response(404)
     return res
