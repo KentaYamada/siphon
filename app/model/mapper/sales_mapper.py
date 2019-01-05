08c273be614a5fdb94c4942dc5c7cae392048fd3
Kenta Yamada <knt.yamada.800@gamil.com>
2019-01-05 23:22:58 +0900

    #82 implement sales_mapper module

---
+++ app/model/mapper/sales_mapper.py
@@ -0,0 +1,145 @@
+import datetime
+from app.model.sales import Sales
+from app.model.mapper.base_mapper import BaseMapper
+
+
+class SalesMapper(BaseMapper):
+    def __init__(self):
+        super().__init__()
+
+    def add(self, sales):
+        if sales is None or not isinstance(sales, Sales):
+            raise ValueError()
+        if not sales.is_valid():
+            return False
+        sales_query = """
+            INSERT INTO sales (
+                sales_date,
+                total_price,
+                discount_price,
+                discount_rate,
+                inclusive_tax,
+                exclusive_tax,
+                deposit
+            ) VALUES (
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+            );
+
+        """
+        sales_item_query = """
+            INSERT INTO sales_items (
+                sales_id,
+                item_no,
+                item_name,
+                unit_price,
+                quantity,
+                subtotal
+            ) VALUES (
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+            );
+        """
+        sales_data = (
+            sales.sales_date,
+            sales.total_price,
+            sales.discount_price,
+            sales.discount_rate,
+            sales.inclusive_tax,
+            sales.exclusive_tax,
+            sales.deposit
+        )
+
+        try:
+            self._db.execute(sales_query, sales_data)
+            sales_id = self._db.fetch_last_row_id()
+            sales_item_data = tuple([(
+                sales_id,
+                item.item_no,
+                item.item_name,
+                item.unit_price,
+                item.quantity,
+                item.subtotal
+            ) for item in sales.items])
+            self._db.bulk_insert(sales_item_query, sales_item_data)
+            self._db.commit()
+            saved = True
+        except Exception as e:
+            self._db.commit()
+            print(e)
+            saved = False
+        return saved
+
+    def cancel(self, sales_id):
+        if sales_id is None or not isinstance(sales_id, int):
+            raise ValueError()
+        if sales_id <= 0:
+            raise ValueError('Invalid sales_id')
+
+        data = (sales_id,)
+        try:
+            self._db.execute_proc('save_cancel_sales', data)
+            self._db.commit()
+            saved = True
+        except Exception as e:
+            self._db.rollback()
+            print(e)
+            saved = False
+        return saved
+
+    def find_monthly_sales(self, year, month):
+        if year is None or not isinstance(year, int):
+            raise ValueError()
+        if month is None or not isinstance(month, int):
+            raise ValueError()
+        query = """
+            SELECT
+                *
+            FROM sales
+            WHERE sales_date >= %s
+              AND sales_date < %s;
+        """
+        data = (
+            datetime.datetime(year, month, 1, 0, 0, 0),
+            datetime.datetime(year, month+1, 1, 0, 0, 0)
+        )
+        rows = None
+        try:
+            rows = self._db.find(query, data)
+            self._db.commit()
+        except Exception as e:
+            self._db.rollback()
+            print(e)
+        return rows
+
+    def find_yearly_sales(self, year):
+        if year is None or not isinstance(year, int):
+            raise ValueError()
+        query = """
+            SELECT
+                *
+            FROM sales
+            WHERE sales_date >= %s
+              AND sales_date < %s;
+        """
+        data = (
+            datetime.datetime(year, 1, 1, 0, 0, 0),
+            datetime.datetime(year+1, 1, 1, 0, 0, 0)
+        )
+        rows = None
+        try:
+            rows = self._db.find(query, data)
+            self._db.commit()
+        except Exception as e:
+            self._db.rollback()
+            print(e)
+        return rows
---
+++ app/tests/model/mapper/test_sales_mapper.py
@@ -0,0 +1,78 @@
+import unittest
+from datetime import datetime
+from app.config import get_db_config
+from app.model.pgadapter import PgAdapter
+from app.model.sales import Sales
+from app.model.sales_item import SalesItem
+from app.model.mapper.sales_mapper import SalesMapper
+
+
+class TestSalesMapper(unittest.TestCase):
+    def setUp(self):
+        self.mapper = SalesMapper()
+
+    def tearDown(self):
+        db = PgAdapter(get_db_config('develop'))
+        query = """
+            TRUNCATE TABLE sales
+            RESTART IDENTITY;
+        """
+        db.execute(query)
+        query2 = """
+            TRUNCATE TABLE sales_items
+            RESTART IDENTITY;
+        """
+        db.execute(query2)
+        db.commit()
+
+    def test_add_ok(self):
+        items = [
+            SalesItem(
+                None,
+                None,
+                i,
+                'test{0}'.format(i),
+                100,
+                1,
+                100
+            )
+            for i in range(1, 11)
+        ]
+        today = datetime.now()
+        sales = Sales(None, today, 100, 0, 0, 0, 0, 100, items)
+        result = self.mapper.add(sales)
+        self.assertTrue(result)
+
+    def test_cancel_ok(self):
+        items = [
+            SalesItem(
+                None,
+                None,
+                i,
+                'test{0}'.format(i),
+                100,
+                1,
+                100
+            )
+            for i in range(1, 11)
+        ]
+        today = datetime.now()
+        sales = Sales(None, today, 100, 0, 0, 0, 0, 100, items)
+        self.mapper.add(sales)
+        result = self.mapper.cancel(1)
+        self.assertTrue(result)
+
+    def test_add_ng_when_ivalid_argment(self):
+        with self.assertRaises(ValueError):
+            self.mapper.add(None)
+            self.mapper.add('a')
+            self.mapper.add(1)
+            self.mapper.add(True)
+
+    def test_cancel_ng_when_invalid_argment(self):
+        with self.assertRaises(ValueError):
+            self.mapper.cancel(None)
+            self.mapper.cancel('a')
+            self.mapper.cancel(1)
+            self.mapper.cancel(True)
+            self.mapper.cancel(0)
