86eb761a4c25ab3503154e002bc35f6da3ca60a1
Kenta Yamada <knt.yamada.800@gamil.com>
2019-01-05 18:25:17 +0900

    fix validation code

--- app/model/sales.py
+++ app/model/sales.py
@@ -1,59 +1,156 @@
-# from app.model.sale_item import SalesItem
+import datetime
+from app.model.base import BaseModel
 
 
-class Sales():
-    def __init__(self, id=None, sales_date=None,
-                 total_price=None, discount_price=None, discount_rate=None,
-                 inclusive_tax=None, exclusive_tax=None, deposit=None,
-                 items=None):
-        self.__id = id
-        self.__sales_date = sales_date
-        self.__total_price = total_price
-        self.__discount_price = discount_price
-        self.__discount_rate = discount_rate
-        self.__inclusive_tax = inclusive_tax
-        self.__exclusive_tax = exclusive_tax
-        self.__deposit = deposit
-        self.__items = items
-        self.__errors = []
+class Sales(BaseModel):
+    def __init__(self, id=None,
+                 sales_date=None, total_price=None,
+                 discount_price=None, discount_rate=None,
+                 inclusive_tax=None, exclusive_tax=None,
+                 deposit=None, items=None, **kwargs):
+        super().__init__()
+        self.id = id
+        self.sales_date = sales_date
+        self.total_price = total_price
+        self.discount_price = discount_price
+        self.discount_rate = discount_rate
+        self.inclusive_tax = inclusive_tax
+        self.exclusive_tax = exclusive_tax
+        self.deposit = deposit
+        self.items = items
 
     @property
-    def errors(self):
-        return self.__errors
+    def id(self):
+        return self.__id
 
-    def validate(self):
-        # todo: decorator化
-        isValid = True
-        if self.__sales_date is None:
-            isValid = False
-            self.__set_error('sales_date', '売上日は必須です')
-        if self.__total_price is None:
-            isValid = False
-            self.__set_error('total_price', '合計金額は必須です')
-        elif self.__total_price <= 0:
-            isValid = False
-            self.__set_error('total_price', '1円未満の合計金額は登録できません')
-        elif self.__total_price < self.__discount_price:
-            isValid = False
-            self.__set_error('discount_price', '合計金額以上の値引額が設定されています')
-        if self.__discount_rate >= 100:
-            isValid = False
-            self.__set_error('discount_rate', '不正な値引率の値です')
-        if self.__deposit is None:
-            isValid = False
-            self.__set_error('deposit', '預かり金は必須です')
-        elif self.__total_price > self.__deposit:
-            isValid = False
-            self.__set_error('deposit', '預かり金が合計金額より少ないです')
-        if self.__items is None or len(self.__items) < 1:
-            isValid = False
-            self.__set_error('items', '明細データがセットされていません')
-        return isValid
+    @id.setter
+    def id(self, value):
+        super()._clear_validation_error('id')
+        if value is not None and not isinstance(value, int):
+            super()._add_validation_error('id', 'IDには整数をセットしてください')
+        else:
+            self.__id = value
 
-    def save(self):
-        if not self.validate():
+    @property
+    def sales_date(self):
+        return self.__sales_date
+
+    @sales_date.setter
+    def sales_date(self, value):
+        super()._clear_validation_error('sales_date')
+        if value is None:
+            super()._add_validation_error(
+                'sales_date',
+                '売上日は必須です'
+            )
+        elif not isinstance(value, datetime.datetime):
+            super()._add_validation_error(
+                'sales_date',
+                '売上日は日付をセットしてください'
+            )
+        else:
+            self.__sales_date = value
+
+    @property
+    def total_price(self):
+        return self.__total_price
+
+    @total_price.setter
+    def total_price(self, value):
+        if value is None:
+            super()._add_validation_error(
+                'total_price',
+                '合計金額は必須です'
+            )
+        elif not isinstance(value, int):
+            super()._add_validation_error(
+                'total_price',
+                '合計金額は整数をセットしてください'
+            )
+        elif value <= 0:
+            super()._add_validation_error(
+                'total_price',
+                '合計金額は0円以上をセットしてください'
+            )
+        else:
+            self.__total_price = value
+
+    @property
+    def discount_price(self):
+        return self.__discount_price
+
+    @discount_price.setter
+    def discount_price(self, value):
+        if not isinstance(value, int):
+            super()._add_validation_error(
+                'discount_price',
+                '値引額は整数をセットしてください'
+            )
+        else:
+            self.__discount_price = value
+
+    @property
+    def discount_rate(self):
+        return self.__discount_rate
+
+    @discount_rate.setter
+    def discount_rate(self, value):
+        if not isinstance(value, int):
+            super()._add_validation_error(
+                'discount_rate',
+                '値引率は整数をセットしてください'
+            )
+        elif value >= 100:
+            super()._add_validation_error(
+                'discount_rate',
+                '無効な値引き率です'
+            )
+        else:
+            self.__discount_rate = value
+
+    @property
+    def deposit(self):
+        return self.__deposit
+
+    @deposit.setter
+    def deposit(self, value):
+        if value is None:
+            super()._add_validation_error(
+                'deposit',
+                '預かり金は必須です'
+            )
+        elif not isinstance(value, int):
+            super()._add_validation_error(
+                'deposit',
+                '預かり金には整数をセットしてください'
+            )
+        else:
+            self.__deposit = value
+
+    def is_valid(self):
+        if not super().is_valid():
+            return False
+        if self.total_price < self.discount_price:
+            super()._add_validation_error(
+                'discount_price',
+                '合計金額以上の値引額が設定されています'
+            )
+            return False
+        if self.deposit < self.total_price:
+            super()._add_validation_error(
+                'deposit',
+                '預かり金が合計金額より少ないです'
+            )
+            return False
+        if self.items is None or len(self.items) == 0:
+            super()._add_validation_error(
+                'items',
+                '明細データがセットされていません'
+            )
             return False
         return True
 
-    def __set_error(self, field, message):
-        self.__errors.append({'name': field, 'message': message})
+    def save(self):
+        if not self.is_valid():
+            return False
+        return True
--- app/tests/model/test_sales.py
+++ app/tests/model/test_sales.py
@@ -1,8 +1,78 @@
 import unittest
+from datetime import datetime
 from app.model.sales import Sales
-from app.model.sales_item import SalesItem
 
 
 class TestSales(unittest.TestCase):
-    def test_single_item_check(self):
-        pass
+    def test_id_validation(self):
+        sales = Sales('1', datetime.now(), 1000, 0, 0, 0, 0, 1000, [])
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('id' in sales.validation_errors.keys())
+
+    def test_sales_date_validation(self):
+        sales = Sales(1, None, 1000, 0, 0, 0, 0, 1000, [])
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('sales_date' in sales.validation_errors.keys())
+
+        sales.sales_date = 'hoge'
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('sales_date' in sales.validation_errors.keys())
+
+    def test_total_price_validation(self):
+        sales = Sales(1, datetime.now(), None, 0, 0, 0, 0, 1000, [])
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('total_price' in sales.validation_errors.keys())
+
+        sales.total_price = '1000'
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('total_price' in sales.validation_errors.keys())
+
+        sales.total_price = 0
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('total_price' in sales.validation_errors.keys())
+
+    def test_discount_price_validation(self):
+        sales = Sales(1, datetime.now(), 1000, '0', 0, 0, 0, 1000, [])
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('discount_price' in sales.validation_errors.keys())
+
+    def test_discount_rate_validation(self):
+        sales = Sales(1, datetime.now(), 1000, 0, '0', 0, 0, 1000, [])
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('discount_rate' in sales.validation_errors.keys())
+
+        sales.discount_rate = 100
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('discount_rate' in sales.validation_errors.keys())
+
+    def test_deposit_validation(self):
+        sales = Sales(1, datetime.now(), 1000, 0, 0, 0, 0, None, [])
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('deposit' in sales.validation_errors.keys())
+
+        sales.deposit = '1000'
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('deposit' in sales.validation_errors.keys())
+
+    def test_total_price_more_than_discount_price(self):
+        sales = Sales(1, datetime.now(), 1000, 1100, 0, 0, 0, 1000, [])
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('discount_price' in sales.validation_errors.keys())
+
+    def test_deposit_less_than_total_price(self):
+        sales = Sales(1, datetime.now(), 1000, 0, 0, 0, 0, 900, [])
+        result = sales.is_valid()
+        self.assertFalse(result)
+        self.assertTrue('deposit' in sales.validation_errors.keys())
