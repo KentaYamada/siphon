56cbcc5fecca93a2d77c7342d677b665243b1144
Kenta Yamada <knt.yamada.800@gamil.com>
2019-01-05 18:41:57 +0900

    fix remove add and cancel method

--- app/tests/model/mapper/test_sales_item_mapper.py
+++ app/tests/model/mapper/test_sales_item_mapper.py
@@ -18,46 +18,8 @@ class TestSalesItemMapper(unittest.TestCase):
         db.execute(query)
         db.commit()
 
-    def test_add_items_ok(self):
-        items = [SalesItem(
-            None,
-            1,
-            i,
-            'Test item{0}'.format(i+1),
-            (1)*100,
-            1,
-            (1)*100)
-            for i in range(1, 11)]
-        result = self.mapper.add_items(items)
-        self.assertTrue(result)
-
-    def test_cancel_ok(self):
-        self.__init_sales_items()
-        result = self.mapper.cancel(3, 1)
-        self.assertTrue(result)
-
     def test_find_by_sales_id_ok(self):
         pass
-
-    def test_add_items_ng_when_empty_item(self):
-        result = self.mapper.add_items([])
-        self.assertFalse(result)
-
-    def test_add_items_ng_when_invalid_value(self):
-        with self.assertRaises(ValueError):
-            self.mapper.add_items(None)
-            self.mapper.add_items('invalid')
-            self.mapper.add_items(100)
-
-    def test_cancel_ng_when_invalid_value(self):
-        with self.assertRaises(ValueError):
-            self.mapper.cancel(None, None)
-            self.mapper.cancel('invalid', 1)
-            self.mapper.cancel(1, 'invalid')
-            self.mapper.cancel('1', 1)
-            self.mapper.cancel(1, '1')
-            self.mapper.cancel(-1, 1)
-            self.mapper.cancel(1, -1)
 
     def test_find_by_sales_id(self):
         with self.assertRaises(ValueError):
@@ -69,8 +31,13 @@ class TestSalesItemMapper(unittest.TestCase):
         items = []
         for i in range(1, 2):
             for j in range(1, 10):
-                items.append(
-                        SalesItem(
-                            None, i, j, 'Test item{0}'.format(j+1),
-                            (j)*100, 1, (j)*100))
+                item = SalesItem(
+                    None,
+                    i,
+                    j,
+                    'Test item{0}'.format(j+1),
+                    j*100,
+                    1,
+                    j*100)
+                items.append(item)
         self.mapper.add_items(items)
