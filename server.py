bf404631484dee171f513df0c79f135900a5fe86
Kenta Yamada <knt.yamada.800@gamil.com>
2019-01-20 13:39:41 +0900

    #88 開発用の設定を読み込んで実行できるようにする

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
+from os import environ
 from app import startup_app
 
 
 if __name__ == '__main__':
+    # set development env
+    environ['APP_TYPE'] = 'development'
     app = startup_app()
-    app.run(host='0.0.0.0', debug=True)
+    app.config.from_object('app.config.DevelopmentConfig')
+    # print(app.__dict__)
+    app.run()
