b84ae6f182800bad6b26cf73d7d856a709944af8
Kenta Yamada <knt.yamada.800@gamil.com>
2019-02-14 00:18:07 +0900

    #36 implement user

---
+++ frontend/src/components/user/edit/script.ts
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import User from '@/entity/user';
+
+
+export default Vue.extend({
+    data() {
+        return {
+        };
+    },
+    methods: {
+        handleSave() {
+        }
+    }
+});
+
---
+++ frontend/src/components/user/list/script.ts
@@ -0,0 +1,42 @@
+import Vue from 'vue';
+import User from '@/entity/user';
+import UserEdit from '@/components/user/edit/UserEdit.vue';
+
+export default Vue.extend({
+    data() {
+        const users = User.getDummyUsers();
+
+        return {
+            users
+        };
+    },
+    computed: {
+        hasItems(): boolean {
+            return this.users.length > 0;
+        }
+    },
+    methods: {
+        handleNew(): void {
+        },
+        handleEdit(user: User): void {
+            this.$modal.open({
+                parent: this,
+                component: UserEdit,
+                hasModalCard: true,
+                props: {
+                    user: user
+                }
+            });
+        },
+        handleDelete(user: User): void {
+            this.$dialog.confirm({
+                title: 'ユーザー削除',
+                message: `<div>${user.name}を削除しますか？<div><small>Note:削除したデータを元に戻すことはできません</small>`,
+                confirmText: '削除',
+                cancelText: '閉じる',
+                hasIcon: true,
+                type: 'is-danger'
+            });
+        }
+    }
+});
