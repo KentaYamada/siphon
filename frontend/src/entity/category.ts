ce936c07b18f1c3b1a7a90f79570e37c77068b4b
Kenta Yamada <knt.yamada.800@gamil.com>
2019-02-27 21:55:51 +0900

    #97 add method

--- frontend/src/entity/category.ts
+++ frontend/src/entity/category.ts
@@ -1,11 +1,15 @@
+import Item from '@/entity/item';
+
 
 export default class Category {
-    public id: number;
-    public name: string;
+    public id: number | null = null;
+    public name: string | null = null;
+    public items: Item[];
 
-    constructor(id: number, name: string) {
+    constructor(id: number, name: string, items: Item[]=[]) {
         this.id = id;
         this.name = name;
+        this.items = items;
     }
 
     public static getDummyCategories(): Category[] {
@@ -16,6 +20,19 @@ export default class Category {
         }
 
         return list;
+    }
+
+    public static getDummyCashierPanel(): Category[] {
+        let categories = [];
+
+        for (let i = 1; i <= 10; i++) {
+            let category = new Category(i, `Category${i}`);
+            category.items = Item.getDummyItems(i);
+            categories.push(category);
+        }
+
+        console.log(categories);
+        return categories;
     }
 }
 
