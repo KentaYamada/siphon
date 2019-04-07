0e595b99134491a1b02df8b2d306ce1cc676903b
Kenta Yamada <knt.yamada.800@gamil.com>
2019-04-07 16:38:48 +0900

    #99 Implement nav bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Dashboard</router-link> |
-      <router-link to="/cashier">Cashier</router-link> |
-      <router-link to="/sales/daily">Daily Sales</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/categories">Category</router-link> |
-      <router-link to="/items">Item</router-link> |
-      <router-link to="/users">User</router-link> |
-    </div>
-    <!-- <navigation/> -->
-    <router-view/>
-    <footer class="footer">
-        <div class="content has-text-centered">
-            Siphon ~ Cafe de GAMOYON ~ 
-        </div>
-    </footer>
+      <navigation/>
+      <router-view/>
+      <footer class="footer">
+          <div class="content has-text-centered">
+              Siphon ~ cafe de GAMOYON ~ 
+          </div>
+      </footer>
   </div>
 </template>
 
@@ -30,24 +21,3 @@ export default Vue.extend({
     }
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-#nav {
-  padding-top: 10px;
-  padding-bottom: 20px;
-  text-align: center;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- frontend/src/components/navigation/script.ts
+++ frontend/src/components/navigation/script.ts
@@ -1,32 +1,50 @@
 import Vue from 'vue';
 
 
+const getMenu = () => {
+    return [
+        {
+            title: '売上',
+            icon: 'fa-coins',
+            submenu: [
+                {title: '売上登録', icon: 'fa-cash-register', url: '/cashier', submenu: null},
+                {title: '1日の売上', icon: 'fa-calendar-day', url: '', submenu: null}
+            ]
+        },
+        {
+            title: '設定',
+            icon: 'fa-user-cog',
+            submenu: [
+                {title: '商品カテゴリ', icon: 'fa-tag', url: '/categories', submenu: null},
+                {title: '商品', icon: 'fa-coffee', url: '/items', submenu: null},
+                {title: 'ユーザー', icon: 'fa-users', url: '/users', submenu: null}
+            ]
+        }
+    ];
+};
+
+
 export default Vue.extend({
     template: '<navigation/>',
     data() {
-        const menu = [
-            {
-                title: '売上',
-                submenu: [
-                    {title: '売上登録', url: '/cashier'},
-                    // {title: '本日の売上', url: '/sales/daily'},
-                ]
-            },
-            {
-                title: '設定',
-                submenu: [
-                    {title: '商品カテゴリ', url: '/categories'},
-                    {title: '商品', url: '/items'},
-                    {title: 'ユーザー', url: '/users'}
-                ]
-            }
-        ];
+        const menu = getMenu();
 
-        return { menu };
+        return {
+            menu,
+            showNav: false
+        };
     },
     computed: {
         isLoggedIn(): boolean {
             return true;
+        }
+    },
+    methods: {
+        toggleNav(): void {
+            this.showNav = !this.showNav;
+        },
+        handleCloseNav(): void {
+            this.showNav = false;
         }
     }
 });
