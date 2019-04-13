a4939740b76106332e50ccab4681baa0bc736e3a
Kenta Yamada <knt.yamada.800@gamil.com>
2019-04-13 20:23:04 +0900

    #92 単体テストスクリプト作成

---
+++ db/tests/test_save_sales.sql
@@ -0,0 +1,12 @@
+-- 新規作成
+SELECT save_sales(CURRENT_DATE, CURRENT_TIME::time, 1000, 0, 0, 0, 0, 1000);
+SELECT save_sales_item(1, 1, 'Siphon coffee', 450, 1, 450);
+SELECT save_sales_item(1, 2, 'ケーキ550', 550, 1, 550);
+
+-- データ確認
+SELECT * FROM sales;
+SELECT * FROM sales_items;
+
+-- データ初期化
+TRUNCATE TABLE sales RESTART IDENTITY;
+TRUNCATE TABLE sales_items RESTART IDENTITY;
