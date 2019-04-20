5f8fa6b085319807dc78df09ce234ea1e541700f
Kenta Yamada <knt.yamada.800@gamil.com>
2019-04-21 00:43:00 +0900

    fix: レジ画面で使う商品カテゴリ、商品データをDBから取得

--- app/controller/cashier.py
+++ app/controller/cashier.py
@@ -3,52 +3,28 @@ from flask import request, Blueprint
 from app.controller.response import ResponseBody
 from app.model.sales import Sales
 from app.model.sales_item import SalesItem
+from app.model.category import CategorySearchOption
+from app.model.item import ItemSearchOption
+from app.model.mapper.category_mapper import CategoryMapper
+from app.model.mapper.item_mapper import ItemMapper
 from app.model.mapper.sales_mapper import SalesMapper
 
 
 bp = Blueprint('cashier', __name__, url_prefix='/api/cashier')
 
 
-def to_dimention_array(arr, row, col):
-    """ 一次元配列から二次元配列へ変換 """
-    res = []
-    offset = 0
-
-    for i in range(0, row):
-        res.append(arr[offset:col+offset])
-        offset += col
-    return res
-
-
-def generatePanel():
-    """ 商品選択パネルダミーデータ生成 """
-    data = {'categories': []}
-    item_id = 1
-    for i in range(1, 11):
-        category = {
-            'id': i,
-            'name': 'Category{}'.format(i),
-            'items': []
-        }
-
-        for j in range(1, 31):
-            category['items'].append({
-                "id": item_id,
-                "name": "Item{}".format(item_id),
-                "unit_price": j * 100
-            })
-            item_id += 1
-        category['items'] = to_dimention_array(category['items'], 10, 3)
-        data['categories'].append(category)
-    data['categories'] = to_dimention_array(data['categories'], 2, 5)
-    return data
-
-
 @bp.route('/', methods=['GET'])
 def index():
+    categories = _get_categories()
+    items = _get_items(categories)
+    for category in categories:
+        data = [i for i in items if category['id'] == i['category_id']]
+        # 3x10の二次元配列へ変換
+        category['items'] = _to_dimention_array(data, 10, 3)
+    # 5x2の二次元配列へ変換
+    categories = _to_dimention_array(categories, 2, 5)
     res = ResponseBody()
-    data = generatePanel()
-    res.set_success_response(200, data)
+    res.set_success_response(200, {'categories': categories})
     return res
 
 
@@ -64,12 +40,17 @@ def add():
         res.set_fail_response(400, message='売上明細データがセットされていません')
         return res
 
-    items = [SalesItem(None, None, i, **item)
-             for i, item in enumerate(request.json['items'], 1)]
+    items = [SalesItem(item_no=i, **item) for i, item in enumerate(request.json['items'], 1)]
     del request.json['items']
     now_date = datetime.now().date()
     now_time = datetime.now().time()
-    sales = Sales(None, sales_date=now_date, sales_time=now_time, items=items, **request.json)
+    sales = Sales(
+        None,
+        sales_date=now_date,
+        sales_time=now_time,
+        items=items,
+        **request.json
+    )
 
     if not sales.is_valid():
         res.set_fail_response(400, sales.validation_errors)
@@ -82,4 +63,48 @@ def add():
         res.set_success_response(201)
     else:
         res.set_fail_response(409)
+    return res
+
+
+def _get_categories():
+    option = CategorySearchOption()
+    mapper = CategoryMapper()
+    rows = mapper.find(option)
+    # 登録件数が10件未満の場合は、空データで埋め合わせ
+    unregist_count = CategoryMapper.MAX_ADDABLE_ROW - len(rows)
+    for i in range(0, unregist_count):
+        rows.append({'id': None, 'name': ''})
+    return rows
+
+
+def _get_items(categories):
+    category_ids = [c['id'] for c in categories]
+    option = ItemSearchOption(
+        category_ids=category_ids
+    )
+    mapper = ItemMapper()
+    rows = mapper.find_by_category_ids(option)
+    items = []
+    # 1カテゴリあたり登録件数が30件未満の場合は、空データで埋め合わせ
+    for c in categories:
+        registered_items = [r for r in rows if c['id'] == r['category_id']]
+        unregist_count = ItemMapper.MAX_ADDABLE_DATA - len(registered_items)
+        for i in range(0, unregist_count):
+            registered_items.append({
+                'id': None,
+                'category_id': c['id'],
+                'name': '',
+                'unit_price': None
+            })
+        items += registered_items
+    return items
+
+
+def _to_dimention_array(arr, row, col):
+    """ 一次元配列から二次元配列へ変換 """
+    res = []
+    offset = 0
+    for i in range(0, row):
+        res.append(arr[offset:col+offset])
+        offset += col
     return res
