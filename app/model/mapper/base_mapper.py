97b5dfa7fa5d99567da3aef6b24ca5c0a83e7fcf
Kenta Yamada <knt.yamada.800@gamil.com>
2019-04-20 23:14:17 +0900

    upd: selectのフォーマットメソッド追加

--- app/model/mapper/base_mapper.py
+++ app/model/mapper/base_mapper.py
@@ -4,3 +4,11 @@ from app.model.pgadapter import PgAdapter
 class BaseMapper:
     def __init__(self):
         self._db = PgAdapter()
+
+    def format_rows(self, rows, field_list):
+        if rows is None or field_list is None:
+            raise ValueError('Invalid argument')
+        if not isinstance(field_list, list):
+            raise ValueError('Invalid data type. arg:fields_list')
+        rows = [{f: row[f] for f in field_list} for row in rows]
+        return rows
