f5412632d4d53ae2c1d6923e49336dd2382b9d1b
Kenta Yamada <knt.yamada.800@gamil.com>
2019-05-04 18:21:00 +0900

    fix: add daily sales api

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.controller import cashier
 from app.controller import category
+from app.controller import daily_sales
 from app.controller import dashboard
 from app.controller import item
 from app.controller import user
@@ -15,6 +16,7 @@ def startup_app():
     blueprints = [
         cashier.bp,
         category.bp,
+        daily_sales.bp,
         dashboard.bp,
         item.bp,
         user.bp,
