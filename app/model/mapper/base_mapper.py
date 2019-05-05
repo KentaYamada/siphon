151d8d936ae9414945064d383006172ca1e87f58
Kenta Yamada <knt.yamada.800@gamil.com>
2019-05-05 13:24:49 +0900

    implement format row

--- app/model/mapper/base_mapper.py
+++ app/model/mapper/base_mapper.py
@@ -12,3 +12,10 @@ class BaseMapper:
             raise ValueError('Invalid data type. arg:fields_list')
         rows = [{f: row[f] for f in fields} for row in rows]
         return rows
+
+    def format_row(self, row, fields):
+        if row is None or fields is None:
+            raise ValueError('Invalid argumant')
+        if not isinstance(fields, (tuple, list)):
+            raise ValueError('Invalid data type')
+        return {f: row[f] for f in fields}
