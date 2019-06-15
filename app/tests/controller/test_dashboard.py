69c4a69cd9d1f2dad9c597eba7459d065e0c3d63
Kenta Yamada <knt.yamada.800@gamil.com>
2019-06-15 19:25:43 +0900

    refactoring

--- app/tests/controller/test_dashboard.py
+++ app/tests/controller/test_dashboard.py
@@ -1,31 +1,23 @@
 import json
-from unittest import TestCase
-from app import startup_app
-from app.model.pgadapter import PgAdapter
+from datetime import datetime
+from app.tests.controller.base import BaseApiTestCase
 
 
-class TestDashboardApi(TestCase):
+class TestDashboardApi(BaseApiTestCase):
     CONTENT_TYPE = 'application/json'
 
     def setUp(self):
-        self.db = PgAdapter()
+        super().setUp()
+        self.endpoint = '/api/dashboard'
 
     def tearDown(self):
         self.db.execute_proc('cleanup_sales')
         self.db.commit()
 
-    @classmethod
-    def setUpClass(cls):
-        app = startup_app()
-        cls.client = app.test_client()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.client = None
-
-    def test_index_status_200(self):
-        self.__init_data()
-        url = '/api/dashboard/{0}/{1}'.format(2019, 5)
+    def test_index(self):
+        self.init_data()
+        today = datetime.now().date()
+        url = '/api/dashboard/{0}/{1}'.format(today.year, today.month)
         res = self.client.get(
             url,
             content_type=self.CONTENT_TYPE
@@ -34,6 +26,6 @@ class TestDashboardApi(TestCase):
         self.assertEqual(200, res.status_code)
         self.assertNotEqual(0, len(body['popular_items']))
 
-    def __init_data(self):
+    def init_data(self):
         self.db.execute_proc('create_test_data_popular_sales_items')
         self.db.commit()
