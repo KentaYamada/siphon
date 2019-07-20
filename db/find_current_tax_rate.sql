0d2a0675dbcd6b48edba96dcc567a4e322b8de62
Kenta Yamada <knt.yamada.800@gamil.com>
2019-07-20 12:24:46 +0900

    #123 fix rename filename

--- db/find_current_tax_rate.sql
+++ db/find_current_tax_rate.sql
@@ -1,16 +1,20 @@
-CREATE OR REPLACE FUNCTION find_current_tax(
+CREATE OR REPLACE FUNCTION find_current_tax_rate (
     p_current_date date
 )
 RETURNS SETOF tax_rates
 LANGUAGE plpgsql
 SECURITY DEFINER
 AS $$
+BEGIN
     RETURN QUERY
     SELECT
+        t.id,
         t.rate,
         t.reduced_rate,
+        t.start_date,
+        t.end_date,
         t.tax_type
     FROM tax_rates AS t
     WHERE t.start_date <= p_current_date
-      AND t.end_date > p_current_date;
+      AND t.end_date >= p_current_date;
 END $$;
