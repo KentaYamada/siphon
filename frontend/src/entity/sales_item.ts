7447bc286ed24b46f838098594386df3830d115b
Kenta Yamada <knt.yamada.800@gamil.com>
2019-09-16 12:35:48 +0900

    #122 upd: プロパティ変更

--- frontend/src/entity/sales_item.ts
+++ frontend/src/entity/sales_item.ts
@@ -1,9 +1,19 @@
+import { TAX_OPTIONS } from '@/entity/tax_rate';
+import { Item } from './item';
+
+
 /**
  * 売上明細モデル
  */
-export default interface SalesItem {
-    item_name: string;
+export interface SalesItem {
+    name: string;
     unit_price: number;
     quantity: number;
     subtotal: number;
+    tax_option: TAX_OPTIONS;
+};
+
+export interface TargetItem {
+    item: Item | SalesItem;
+    tax_option: TAX_OPTIONS;
 }
